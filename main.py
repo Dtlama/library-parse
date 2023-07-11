9dea39882e7666f27961beb79b5dc7b2b31a68d6
Dtlama <serebryakovss1901@gmail.com>
2023-07-11 12:34:51 +0300

    download book covers

--- main.py
+++ main.py
@@ -40,20 +40,35 @@ def get_book_image(url):
     response.raise_for_status()
     soup = BeautifulSoup(response.text, 'lxml')
     image_url = soup.find('body').find('table').find('div', class_='bookimage').find('img')['src']
+    image_url = 'https://tululu.org' + image_url
     return image_url
 
-# url_download = 'http://tululu.org/txt.php?id=1'
-# file_path = download_book(url_download, 'Алиби')
-# print(file_path)
 
+def download_image(url, filename, folder="images/"):
+    response = requests.get(url)
+    check_for_redirect(response)
+    response.raise_for_status()
+    if url == "https://tululu.org/images/nopic.gif":
+        filename = 'nopic.gif'
+    else:
+        filename = f'{filename}.jpg'
+    file_path = os.path.join('images', filename)
 
+    with open(file_path, 'wb') as file:
+        file.write(response.content)
+    return file_path
+
+#https://tululu.org/b5/
 if __name__ == '__main__':
     os.makedirs('books', exist_ok=True)
+    os.makedirs('images', exist_ok=True)
     for number in range(1, 11):
         try:
             url = 'https://tululu.org/b{}/'.format(number)
             book_name, book_author = get_book_name_and_author(url)
             image_url = get_book_image(url)
+            print(image_url)
+            download_image(image_url, number)
             url_download = 'http://tululu.org/txt.php?id={}'.format(number)
             file_path = download_book(url_download, book_name)
             print(book_name, image_url)
